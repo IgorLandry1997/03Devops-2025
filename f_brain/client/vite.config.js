9450868c3a66d10376cd0754961208e1863337a5
IgorLandry1997 <njoungangigor@gmail.com>
2025-06-02 13:24:33 +0100

    Work in progress before completing revert

--- f_brain/client/vite.config.js
+++ f_brain/client/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // <== virgule ajoutée ici
-    port: 5173,
-  }
 })
