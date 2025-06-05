d0ae0a35496d0086c51d98898404c2a54843c011
IgorLandry1997 <njoungangigor@gmail.com>
2025-06-05 18:52:02 +0100

    WeatherCard component updated

--- f_brain/client/src/components/WeatherCard.jsx
+++ f_brain/client/src/components/WeatherCard.jsx
@@ -1,9 +1,15 @@
 import "./WeatherCard.css";
 
 export default function WeatherCard({ data }) {
+  // URL de base pour les icônes météo d'OpenWeatherMap
+  const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
   return (
     <div className="weather-card">
-      <h2>{data.name}</h2>
+      <h2>{data.name} {data.sys.country && <span className="country">({data.sys.country})</span>}</h2>
+      <div className="weather-icon">
+        <img src={iconUrl} alt={data.weather[0].description} />
+      </div>
       <p>{data.weather[0].description}</p>
       <div className="temperature">{Math.round(data.main.temp)}°C</div>
       <p>Feels like {Math.round(data.main.feels_like)}°C</p>
